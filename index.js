1d677eb19e9f06259fd696391e5be2024dfbbfa4
piperpei <piperpei@yahoo.com>
2025-08-21 16:47:55 -0700

    Login Tracker and Arrow Functions

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
+// Create Login Tracker 
 
+function createLoginTracker(userInfo) {
+  // Variables
+  let attemptCount = 0; 
+  const maxAttempts = 3;
+  let locked = false; 
+
+  // Arrow Function 
+  const attmeptLogin = (passwordAttempt) => {
+    // Check if Locked
+    if (locked) {
+      return 'Account Locked After Three Failed Attempts';
+    }
+
+    // Add Attempts 
+    attemptCount++; 
+
+    // Check if Password Matches 
+    if (passwordAttempt === userInfo.password) {
+      // Reset if Correct 
+      attemptCount = 0;
+      return 'Login Successful';
+
+    // Password Failure 
+    if (attemptCount >= maxAttempts)
+    }
+
+  };
+
+  // Return 
+  return attmeptLogin;  
+
+}
 
 
 module.exports = {
